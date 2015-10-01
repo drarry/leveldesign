9933520f7bd0319d2bdba30df1bc84a5ade06273
Gabby Llanillo <llani100@mail.chapman.edu>
2015-09-30 19:04:29 -0700

    Spring Level Layout

---
+++ Petals/Assets/Scripts/BallControl 1.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var rotationSpeed = 100;
+var jumpHeight = 8;
+
+private var isFalling = false;
+
+function Update ()
+{
+	//Handle ball rotation.
+	var rotation : float = Input.GetAxis ("Horizontal") * rotationSpeed;
+	rotation *= Time.deltaTime;
+	GetComponent.<Rigidbody>().AddRelativeTorque (Vector3.back * rotation);
+	
+	if (Input.GetKeyDown(KeyCode.Space) && isFalling == false)
+	{
+		GetComponent.<Rigidbody>().velocity.y = jumpHeight;
+	}
+	isFalling = true;
+}
+
+function OnCollisionStay ()
+{
+	isFalling = false;
+}
